c1485ffecfc9a6935d6997ee1de1de3360ea271f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-26 12:36:22 -0700

    add basic js

---
+++ index.js
@@ -0,0 +1,41 @@
+//grab the elements
+
+// const randomText = document.getElementById("textBox");
+//  const goBtn = document.getElementById("go-btn");
+//  let resultDiv = document.getElementById("parentDiv");
+// const result = document.createElement("p");
+
+// create a way for the words in any random sentence to be automatically capitalized.
+
+
+let newArray = [];
+
+
+function accumulate(text) {
+    text.toLowerCase()
+       .forEach((letter) => {
+           acc = [i] + 1;
+           newArray.push('-' + letter.charAt(0).toUpperCase() + letter.repeat(acc))
+       })
+    }
+    
+    randomText = "jYfhesoKfjjsd"
+
+    console.log(accumulate(randomText));
+
+    
+    //     resultDiv = document.getElementById("parentDiv");
+    //     resultDiv.innerHTML="";
+    //          for (let i = 0; i < newArray.length; i++) {
+    //              const aWord = document.createElement('div')
+    //              aWord.textContent = newArray[i] + " ";
+    //              resultDiv.append(aWord)
+    //      }})      
+
+    // }
+
+// goBtn.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     return (accumulate(randomText.value));
+// })
+
