6a3ffa3e1c7b636026ceda6711c86f9eafc6170a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-26 12:57:31 -0700

    log the array to the console

--- index.js
+++ index.js
@@ -8,19 +8,19 @@
 // create a way for the words in any random sentence to be automatically capitalized.
 
 
-//let newArray = [];
+let newArray = [];
 
 
-//function accumulate(text) {
-    //newArray.push(text);
-    console.log("Hai");
+function accumulate(text) {
+    newArray.push(text);
+    console.log(newArray);
     // acc = i + 1;
     // text.toLowerCase()
 
     //    for(letter of text) { 
     //        newArray.push("-" + letter.charAt(0).toUpperCase() + letter.repeat(acc))
     //    }
-     //}
+    }
     
     //randomText = "jYfhesoKfjjsd";
 
